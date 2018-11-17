8ab9e3f7eac71312e0c5325a5ad2a3683fa0021d
ArrogantL <1336816438@qq.com>
2018-11-17 23:53:01 +0800

    sucess install CRF++

---
+++ Lab3/ME.py
@@ -0,0 +1,49 @@
+def get_features(trainfile_path,featuresfile_path):
+    """
+    get feature.txt with trainfile
+    :param trainfile_path:
+    :param featuresfile_path:
+    :return:
+    """
+    pass
+
+
+
+
+def train_ME(featuresfile_path,ME_model_path):
+    """
+    get ME_model.txt with feature.txt
+    :param featuresfile_path:
+    :param ME_model_path:
+    :return:
+    """
+    pass
+
+
+
+def getResult(testfile_path,ME_model_path,test_feature_path,result_path):
+    """
+    get test_feature.txt result.txt with model.txt and testfile
+    :param testfile_path:
+    :param ME_model_path:
+    :param test_feature_path:
+    :param result_path:
+    :return:
+    """
+    pass
+
+def evaluate(standard_answer_path,result_path,evaluation_result_path):
+    """
+    get evaluation_result.txt with standard_answer and result.txt by SharedTaskEval.pl
+    :param standard_answer_path:
+    :param result_path:
+    :param evaluation_result_path:
+    :return:
+    """
+
+    pass
+def read_features_file(featuresfile_path):
+    return features
+
+def read_ME_model(ME_model_path):
+    return ME_model
